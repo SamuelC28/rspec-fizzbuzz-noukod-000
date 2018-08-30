d3c73f3e1aecae7b349f417c04a327d061f12159
Samuel Cornet <samuel@noukod.com>
2018-08-30 20:29:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,12 @@
-require 'pry'# Don't forget! This file needs to be 'required' in its spec file
 def fizzbuzz (int)
-  binding.pry
+  
   if int %3 == 0
     puts "Fizz"
-    elsif int %5 == 0
-      puts "Buzz"
-      elsif (int %3 == 0) && (int %5 == 0)
-        puts "FizzBuzz"
-        elsif (int %3 != 0) && (int %5 != 0)
-          puts "nil"
-        end
-      end
-    end
+  elsif int %5 == 0
+    puts "Buzz"
+  elsif (int %3 == 0) && (int % 5 == 0)
+    puts "FizzBuzz"
+  else 
+    puts "nil"
   end
 end
-# See README.md for instructions on how to do this
